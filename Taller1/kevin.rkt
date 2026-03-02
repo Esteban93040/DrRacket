e48c17b51eb04799a0fed98f73670f8f1d20f886
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2026-03-01 21:14:17 -0500

    ejercicio 10

--- Taller1/kevin.rkt
+++ Taller1/kevin.rkt
@@ -1,1 +1,29 @@
 #lang eopl
+
+;Ejercicio 10
+(define auxbalanced
+    (lambda (lst n1 n2)
+        (if (null? lst)
+            (= n1 n2)
+            (if (> n2 n1)
+                #f
+                (if (equal? (car lst) 'O)
+                    (auxbalanced (cdr lst) (+ n1 1) n2)
+                    (if (equal? (car lst) 'C)
+                        (auxbalanced (cdr lst) n1 (+ n2 1))
+                        (auxbalanced (cdr lst) n1 n2)
+                    )
+                )
+            )
+        )
+    )   
+)
+
+(define balanced-parentheses? 
+    (lambda (lst)
+        (if (null? lst)
+            #f
+            (auxbalanced lst 0 0)
+        )
+    )
+)
